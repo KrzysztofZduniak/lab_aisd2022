c3049d346c9e5054efd6a4952bfa9d43d8d90ee3
Krzysztof Zduniak <krzysz.zdun@gmail.com>
2022-03-15 22:55:09 +0100

    indeksy są od zera, cnie

--- sortowanie/selection.py
+++ sortowanie/selection.py
@@ -1,7 +1,7 @@
 def selection(A):
-    for j in range(len(A), 2, -1):
+    for j in range(len(A) - 1, 0, -1):
         max = j
-        for i in range(j - 1, 1, -1):
+        for i in range(j - 1, -1, -1):
             if A[i] > A[max]:
                 max = i
         A[j], A[max] = A[max], A[j]
