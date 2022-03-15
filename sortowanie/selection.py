d05020e54a1094d6394954e5fc049620d3649cc9
Krzysztof Zduniak <krzysz.zdun@gmail.com>
2022-03-15 22:18:04 +0100

    insertion

---
+++ sortowanie/selection.py
@@ -0,0 +1,8 @@
+def selection(A):
+    for j in range(len(A), 2, -1):
+        max = j
+        for i in range(j - 1, 1, -1):
+            if A[i] > A[max]:
+                max = i
+        A[j], A[max] = A[max], A[j]
+    return A
