787d95e7b26ab74179c2d9e835c25c4bd55f4efe
Krzysztof Zduniak <krzysz.zdun@gmail.com>
2022-03-23 22:38:27 +0100

    quicksort benchmark

---
+++ sortowanie/bench_3.py
@@ -0,0 +1,47 @@
+from numpy.lib.function_base import average
+from quick import quicksort
+import matplotlib.pyplot as plt
+import statistics
+import random
+import time
+
+
+def a_shaped_list(n):
+    A = []
+    for i in range(0, n, 2):
+        A.append(i)
+    for i in range(n - 1, 0, -2):
+        A.append(i)
+    return A
+
+
+def bench(sort, A, pivot):
+    startTime = time.time()
+    sort(A, pivot)
+    return time.time() - startTime
+
+
+middle = []
+right = []
+rand = []
+n = list(range(600, 1000, 5))
+
+for i in n:
+    A = a_shaped_list(i)
+    middle.append(bench(quicksort, A, lambda p, r: (p + r) // 2))
+    right.append(bench(quicksort, A, lambda _, r: r))
+
+    # r = statistics.fmean(
+    #     [bench(quicksort, A, lambda p, r: random.randint(p, r)) for _ in range(10)]
+    # )
+    # rand.append(r)
+    rand.append(bench(quicksort, A, lambda p, r: random.randint(p, r)))
+
+
+fig, ax = plt.subplots()
+ax.plot(n, right, label="klucz skrajnie prawy")
+ax.plot(n, middle, label="klucz środkowy")
+ax.plot(n, rand, label="klucz losowy")
+ax.set(xlabel="n", ylabel="time", title="QuickSort")
+ax.legend()
+plt.show()
