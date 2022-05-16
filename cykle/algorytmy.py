bfe7ba4eb340a730d92bcb937ac70185e934df94
Krzysztof Zduniak <krzysz.zdun@gmail.com>
2022-05-16 22:32:31 +0200

    Add algorithms for Euler and Hamilton cycle's

---
+++ cykle/algorytmy.py
@@ -0,0 +1,41 @@
+def euler(G, C=[], v=0):
+    for w, i in enumerate(G[v]):
+        if i == 1:
+            G[v][w] = 2
+            G[w][v] = 2
+            euler(G, C, w)
+    C.append(v)
+
+
+def hamilton(G):
+    V = []
+    end = False
+
+    def hamilton_inner(G, v=0):
+        V.append(v)
+        for w, i in enumerate(G[v]):
+            if i == 1 and w not in V:
+                hamilton_inner(G, w)
+
+        nonlocal end
+        if not end:
+            if set(V) == set(range(len(G))) and G[0][v] == 1:
+                end = True
+            else:
+                V.remove(v)
+
+    hamilton_inner(G)
+    return V
+
+
+G = [
+    [0, 1, 1, 0, 0, 0],
+    [1, 0, 1, 1, 1, 0],
+    [1, 1, 0, 1, 1, 0],
+    [0, 1, 1, 0, 1, 1],
+    [0, 1, 1, 1, 0, 1],
+    [0, 0, 0, 1, 1, 0],
+]
+
+result = hamilton(G)
+print(*list(map(lambda x: x + 1, result)))
