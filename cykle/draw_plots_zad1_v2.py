ac6c6f5c28343b07c612445e964c78edbe5afc96
JakubWieclaw <jwieclaw000@gmail.com>
2022-05-19 17:14:29 +0200

    add correct graphs creation code

---
+++ cykle/draw_plots_zad1_v2.py
@@ -0,0 +1,38 @@
+from itertools import cycle
+#from algorytmy import euler, hamilton, euler_cycle
+import networkx as nx
+import matplotlib.pyplot as plt
+from time import time
+import os
+import sys
+import random
+import pandas as pd
+#from functions import make_undirected_graph, measure_time, egdes_reverse
+
+sys.setrecursionlimit(100000)
+
+
+def draw_plot(nodes, eu, ham, title):
+    plt.figure(figsize=(16, 9))
+    plt.xlabel("Number of nodes")
+    plt.ylabel("Time [s]")
+    plt.plot(nodes, eu, label="Euler cycle")
+    plt.plot(nodes, ham, label="Hamilton cycle")
+    plt.legend(loc="upper left")
+    plt.title(
+        f"Euler and first Hamilton cycle: {title}%")
+    #plt.xlim(start, end)
+    os.makedirs(os.path.join("wykresy", "zadanie_1"), exist_ok=True)
+    plt.savefig(os.path.join("wykresy", "zadanie_1", f'lin_{title}.png'))
+    plt.yscale("log")
+    plt.savefig(os.path.join("wykresy", "zadanie_1", f'log_{title}.png'))
+    plt.clf()
+
+
+eu = pd.read_csv('data_euler.csv')
+ham = pd.read_csv('data_hamilton.csv')
+
+draw_plot(eu["points"], eu["30"], ham["30"],
+          "30")
+draw_plot(eu["points"], eu["70"], ham["70"],
+          "70")
