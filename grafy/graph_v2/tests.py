d9663c75c8511e5037aeb45a9dd5dc9accbfc50e
Krzysztof Zduniak <krzysz.zdun@gmail.com>
2022-05-19 19:08:26 +0200

    Add tests from file for mst

--- grafy/graph_v2/tests.py
+++ grafy/graph_v2/tests.py
@@ -3,21 +3,40 @@ from graph_generation import (
     mst3 as mst,
     sort_top2 as top,
 )
+import matplotlib.pyplot as plt
+import networkx as nx
 
-txt = open("input")
-n = txt.readline()
-undirected = MatrixGraph(int(n))
-directed = MatrixGraph(int(n), directed=True)
-for v, row in enumerate(map(lambda x: list(x.split()), txt.readlines())):
+
+txt = open("mst_test.txt")
+matrix = list(map(lambda x: list(map(int, x.split())), txt.readlines()))
+undirected = MatrixGraph(len(matrix))
+G = nx.Graph()
+for v, row in enumerate(matrix):
     for w, val in enumerate(row):
         try:
-            directed.add_edge(int(v), int(w), weight=int(val))
-            undirected.add_edge(int(v), int(w), weight=int(val))
+            undirected.add_edge(v, w, weight=val)
         except RuntimeError:
             continue
+        if val != 0:
+            G.add_edge(v, w, pos=(v, w), weight=val)
 
-for x in undirected.n_and_e:
-    print(*x)
 
-mst(undirected)
-top(directed)
+t = mst(undirected)
+print(t)
+
+sum = 0
+for v, w in t:
+    sum += undirected.n_and_e[v][w]
+print(sum)
+
+pos = nx.spring_layout(G)
+k = G.edge_subgraph(t)
+labels = {e: G.edges[e]["weight"] for e in G.edges()}
+plt.subplot(121)
+nx.draw_networkx(G, pos, with_labels=True)
+nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
+
+plt.subplot(122)
+nx.draw_networkx(k, pos=pos, node_color="r")
+
+# plt.show()
