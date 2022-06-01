481388fa4e702a79a6c56b015461e1c67d3f2aef
JakubWieclaw <jwieclaw000@gmail.com>
2022-06-01 03:46:52 +0200

    update item class

--- plecak/class_item.py
+++ plecak/class_item.py
@@ -4,3 +4,9 @@ class Item:
         self.weight = weight
         self.value  = value
         self.profit = self.value/self.weight
+
+    def __str__(self):
+        return f"Item(weight={self.weight},value={self.value})"
+
+    def __repr__(self):
+        return self.__str__()
