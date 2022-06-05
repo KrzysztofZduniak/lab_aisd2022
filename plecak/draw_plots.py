8882160c9c35c6c018815953f9277eb5d8cf8b00
JakubWielcaw <jwieclaw000@gmail.com>
2022-06-05 23:10:35 +0200

    add multiprocessing and error graph

--- plecak/draw_plots.py
+++ plecak/draw_plots.py
@@ -35,18 +35,39 @@ def draw_plot_variable_capacity(x_axis, y_axis_greedy, y_axis_dynamic):
     plt.clf()
 
 
-df_greedy = pd.read_csv(os.path.join("greedy",
+df_greedy_1 = pd.read_csv(os.path.join("greedy",
                                      "greedy_with_variable_items.csv"))
 df_dynamic = pd.read_csv(os.path.join("dynamic",
                                       "dynamic_with_variable_items.csv"))
 
 draw_plot_variable_items(
-    df_greedy["points"], df_greedy["time"], df_dynamic["time"])
+    df_greedy_1["points"], df_greedy_1["time"], df_dynamic["time"])
 
-df_greedy = pd.read_csv(os.path.join("greedy",
+df_greedy_2 = pd.read_csv(os.path.join("greedy",
                                      "greedy_with_variable_capacity.csv"))
 df_dynamic = pd.read_csv(os.path.join("dynamic",
                                       "dynamic_with_variable_capacity.csv"))
 
 draw_plot_variable_capacity(
-    df_greedy["points"], df_greedy["time"], df_dynamic["time"])
+    df_greedy_2["points"], df_greedy_1["time"], df_dynamic["time"])
+
+
+def draw_error(x_axis, y_axis_greedy, y_axis_dynamic):
+    plt.figure(figsize=(16, 9))
+    plt.xlabel("Measure point")
+    plt.ylabel("Error")
+    plt.plot(x_axis, y_axis_greedy, label="Const items")
+    plt.plot(x_axis, y_axis_dynamic, label="Const capacity")
+    plt.legend(loc="upper left")
+    plt.title("Greedy algorithm error")
+    os.makedirs(os.path.join("plots", "error"), exist_ok=True)
+    plt.savefig(os.path.join(
+        "plots", "error", "lin_error.png"))
+    plt.yscale("log")
+    plt.savefig(os.path.join(
+        "plots", "error", "log_error.png"))
+    plt.clf()
+
+df_error = [range(1,16), df_greedy_1["error"], df_greedy_2["error"]]
+
+draw_error(df_error[0], df_error[1], df_error[2])
--- plecak/test_all_fun.py
+++ plecak/test_all_fun.py
@@ -3,32 +3,41 @@ from dynamic import dynamic_knapsack
 from class_item import Item
 import random
 from time import perf_counter
+import multiprocessing
 
+def do(x):
+    number_of_items, capacity, tries, end_range_of_radnomization = x
+    print(
+        f"{number_of_items} items     {capacity} capacity     xd/{tries} attempt")
+    all_items = [Item(random.randint(1, end_range_of_radnomization), random.randint(
+        1, end_range_of_radnomization)) for _ in range(number_of_items)]
 
-def measure_parameters(number_of_items, capacity, tries, end_range_of_radnomization):
+    start = perf_counter()
+    params_dynamic = dynamic_knapsack(number_of_items, capacity, all_items)
+    end = perf_counter()
+    dyn_time = end-start
+    
+
+    start = perf_counter()
+    params_greedy = greedy_knapsack(all_items, number_of_items, capacity)
+    end = perf_counter()
+    greedy_time = end-start
+    
+    return (params_dynamic[0], dyn_time, params_greedy[0], greedy_time)
+
+def measure_parameters(number_of_items, capacity, tries, end_range_of_radnomization, pool):
 
     dyn_time = 0
     greedy_time = 0
     dyn_val = 0
     greedy_val = 0
 
-    for i in range(tries):
-        print(
-            f"{number_of_items} items     {capacity} capacity     {i+1}/{tries} attempt")
-        all_items = [Item(random.randint(1, end_range_of_radnomization), random.randint(
-            1, end_range_of_radnomization)) for _ in range(number_of_items)]
-
-        start = perf_counter()
-        params_dynamic = dynamic_knapsack(number_of_items, capacity, all_items)
-        end = perf_counter()
-        dyn_time += end-start
-        dyn_val += params_dynamic[0]
-
-        start = perf_counter()
-        params_greedy = greedy_knapsack(all_items, number_of_items, capacity)
-        end = perf_counter()
-        greedy_time += end-start
-        greedy_val += params_greedy[0]
+    
+    vals = pool.map(do, [(number_of_items, capacity, tries, end_range_of_radnomization)] * tries)
+    dyn_time = sum(t[1] for t in vals)
+    greedy_time = sum(t[3] for t in vals)
+    dyn_val = sum(t[0] for t in vals)
+    greedy_val = sum(t[2] for t in vals)
 
     dyn_time = dyn_time/tries
     greedy_time = greedy_time/tries
